74efe07f1483b444ce2ab014291363efc3ee44c5
jimno <menaoir.jimmy@gmail.com>
2022-04-15 15:02:18 +0100

    js prompt cancle fixed

--- script.js
+++ script.js
@@ -18,43 +18,43 @@ function roundResult(computerSelection, playerSelection){
     if (computerSelection.toLowerCase() === playerSelection.toLowerCase())
         return `Same brain! You both chose ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()}`;
     else if (computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "scissors"){
-        cWinner = "you lose, Rock beats Scissors";
+        cWinner = "You lose, Rock beats Scissors";
         cpuScore++;
         return cWinner}
     else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "paper"){
-        cWinner = `you lose, Scissors beat Paper`;
+        cWinner = `You lose, Scissors beat Paper`;
         cpuScore++;
         return cWinner}
     else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock"){
-        cWinner = `you lose, Paper beats Rock`;
+        cWinner = `You lose, Paper beats Rock`;
         cpuScore++;
         return cWinner}
     else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors"){
-        pWinner = `you win, Rock beats Scissors`;
+        pWinner = `You win, Rock beats Scissors`;
         playerScore++;
         return pWinner}
     else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper"){
-        pWinner = `you win, Scissors beat Paper`;
+        pWinner = `You win, Scissors beat Paper`;
         playerScore++;
         return pWinner}
     else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock"){
-        pWinner= `you win, Paper beats Rock`;
+        pWinner= `You win, Paper beats Rock`;
         playerScore++;
         return pWinner}
     else 
-    return 'what the Heck did you just chose!';   
+    return 'What the Heck did you just chose!';   
 }
-
-
-
 
 
 
 function game(){
     for (let i = 0; i < Infinity; i++) {
-        const playerSelection = prompt("Please choose one: Rock, Paper or Scissors", "Rock");
         const computerSelection = computerPlay();
-        console.log(roundResult(computerSelection , playerSelection));
+        const playerSelection = prompt("Please choose one: Rock, Paper or Scissors", "Rock, Paper or Scissors");
+        if (playerSelection === null) {
+            return "Okay, maybe next time";
+        }
+        alert(roundResult(computerSelection , playerSelection));
       
         if (cpuScore === 5){
         break;   
@@ -71,4 +71,4 @@ function game(){
     else
     return ""   
 }
-console.log(game())
+alert(game())
