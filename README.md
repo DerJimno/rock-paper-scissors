75096b7e3da55a701dd21f5e92fb4c83101703af
Jimno <menaoir.jimmy@gmail.com>
2022-06-06 17:03:14 +0100

    created and designed choices viewer with some gifs

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 # scissors icon by plutopixel, https://freeicons.io/profile/14783
 # Instagram icon by icon king1, https://freeicons.io/profile/3
 # Facebook icon by radj dev, https://freeicons.io/profile/714
-
+# free loading icon from https://gifer.com
 # backgound image by Markus Spiske https://www.pexels.com/@markusspiske/
 
 # play again image by andregunawan https://github.com/andregunawan/TriviaGame/blob/master/assets/images/playAgain.png
--- script.js
+++ script.js
@@ -13,28 +13,45 @@ let playerScore = 0
 
 function roundResult(computerSelection, playerSelection) {
 
+    const youChoice = document.querySelector(".youChoice")
+    const cpuChoice = document.querySelector(".cpuChoice")
+
     const score = document.querySelector('#score')
 
     if (computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
         score.textContent = "{{{it's a tie}}}";}
     else if (computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "scissors"){
         cpuScore++;
+        cpuChoice.style = "transform: scale(1.6); transition: 1s ;";
+        youChoice.style = "transform: scale(1.0); transition: 1s ;";
         score.textContent = "{{{You lost, your Scissors suck}}}";}
     else if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "paper"){
         cpuScore++;
+        cpuChoice.style = "transform: scale(1.6); transition: 1s ;";
+        youChoice.style = "transform: scale(1.0); transition: 1s ;";
         score.textContent = "{{{You lost, Paper is just Paper}}}";}
     else if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock"){
         cpuScore++;
+        cpuChoice.style = "transform: scale(1.6); transition: 1s ;";
+        youChoice.style = "transform: scale(1.0); transition: 1s ;";
         score.textContent = '{{{You lose, who said "rock-solid"?!}}}';}
     else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors"){
         playerScore++;
+        youChoice.style = "transform: scale(1.6); transition: 1s ;";
+        cpuChoice.style = "transform: scale(1.0); transition: 1s ;";
         score.textContent = "{{{You won, broke those Scissors}}}";}
     else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper"){
         playerScore++;
+        youChoice.style = "transform: scale(1.6); transition: 1s ;";
+        cpuChoice.style = "transform: scale(1.0); transition: 1s ;";
         score.textContent = "{{{You win, cut it Baby}}}";}
     else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock"){
         playerScore++;
+        youChoice.style = "transform: scale(1.6); transition: 1s ;";
+        cpuChoice.style = "transform: scale(1.0); transition: 1s ;";
         score.textContent = "{{{You win, Paper is key! }}}";}
+
+        
 }
 
 
@@ -70,20 +87,43 @@ function reloadButton() {
         selection.style = "visibility: hidden;"
     })
  }
-        
+
 if (cpuScore === 5){
     score.textContent = " Game Over, Loser 😛 ";
-    computerScore.style = "transform: scale(1.5); transition: 160ms; color: red; "
-    score.style = "transform: scale(1.5); transition: 160ms ; "
+    computerScore.style = "transform: scale(1.5); transition: 160ms; color: red; ";
+    score.style = "transform: scale(1.5); transition: 160ms ; ";
     reloadButton()
+    
 }
 else if (playerScore === 5){
     score.textContent = " We have a winner 👌 ";
-    youScore.style = "transform: scale(1.5); transition: 160ms; color: green; "
-    score.style = "transform: scale(1.5); transition: 160ms ; "
+    youScore.style = "transform: scale(1.5); transition: 160ms; color: green; ";
+    score.style = "transform: scale(1.5); transition: 160ms; ";
     reloadButton()
+    
 };    
 
+const youChoice = document.querySelector(".youChoice")
+ if (img.id === "Paper") {
+     youChoice.src = "icons/paper.png"
+ }
+ else if (img.id === "Rock") {
+     youChoice.src = "icons/rock.png"
+ }
+ if (img.id === "Scissors") {
+     youChoice.src = "icons/scissors.png"
+ }
+
+const cpuChoice = document.querySelector(".cpuChoice")
+ if (computerSelection === "Paper") {
+     cpuChoice.src = "icons/paper.png"
+ }
+ else if (computerSelection === "Rock") {
+     cpuChoice.src = "icons/rock.png"
+ }
+ if (computerSelection === "Scissors") {
+     cpuChoice.src = "icons/scissors.png"
+ }
 
     }) 
     })
